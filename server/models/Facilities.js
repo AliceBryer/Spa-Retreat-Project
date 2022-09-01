557c79d0eaa15418304eba59a890eead928ff5a7
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-01 20:11:49 +0100

    completed facilities.js model

--- server/models/Facilities.js
+++ server/models/Facilities.js
@@ -1,3 +1,18 @@
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
+
+const facilitiesSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  imageURL: {
+    type: String,
+  },
+});
+
+const Product = mongoose.model("Facilities", facilitiesSchema);
+
+module.exports = Facilities;
