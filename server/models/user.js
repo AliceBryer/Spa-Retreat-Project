0f0c43d4b993034fecc1642edf0b774b5cb199be
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-01 20:03:40 +0100

    added functions password and email

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,30 @@
 const mongoose = require ('mongoose');
 
 const { Schema } = mongoose;
+const bcrypt = require ('bcrypt');
+const Order = require ('./Orders');
+
+const userSchema = new Schema({
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
+    orders: [Order.schema]
+  });
+  
