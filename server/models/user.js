6227928e139db000401d9e3d8e930cf0d328efaf
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-01 20:04:09 +0100

    created models for user

--- server/models/user.js
+++ server/models/user.js
@@ -28,3 +28,19 @@ const userSchema = new Schema({
     orders: [Order.schema]
   });
   
+  userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  userSchema.methods.isCorrectPassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+  };
+  
+  const User = mongoose.model('User', userSchema);
+  
+  module.exports = User;
