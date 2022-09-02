368ebfd854712c36e68d34fbf50b36d1908d6b94
KeyurMistry <keyurmist@gmail.com>
2022-09-02 19:26:47 +0100

    created footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer>
+      <h5>©Team Super Awesome</h5>
+      <div className="footer">
+        <ul>
+          <li>
+            <NavLink to="/AboutUs">About Us</NavLink>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
