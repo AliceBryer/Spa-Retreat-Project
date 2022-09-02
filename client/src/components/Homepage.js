defd13b98bbcbe666ecc8bd86635fb02da0cb239
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-02 15:59:44 +0100

    header's content and css done (for now)

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Homepage extends Component {
+  render() {
+    return (
+      <section id="home-page">
+        <div>
+          <p>This is home page</p>
+        </div>
+      </section>
+    );
+  }
+}
