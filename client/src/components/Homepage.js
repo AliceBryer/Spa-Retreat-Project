4637161330c6104af8ec8d5725f3e4fc4c3f0a0c
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-02 17:46:39 +0100

    homepage content and css done

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,13 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
+import candle from "../img/candle.avif";
+import diffuser from "../img/diffuser.avif";
+import massage from "../img/massage.avif";
+import oil from "../img/oil.avif";
+import relax from "../img/relax.avif";
 
-export default class Homepage extends Component {
-  render() {
-    return (
-      <section id="home-page">
+const homepagePics = [candle, massage, diffuser, massage, oil, relax];
+
+function Homepage() {
+  const [currentImage, setCurrentImage] = useState(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImage(
+        homepagePics[Math.floor(Math.random() * homepagePics.length)]
+      );
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <section id="home-page">
+      <div className="homepage-pic-container">
         <div>
-          <p>This is home page</p>
+          <img className="welcome-pic" src={currentImage} alt="spa pictures" />
         </div>
-      </section>
-    );
-  }
+        <div className="welcome-msg">Welcome to 404GetAway</div>
+      </div>
+    </section>
+  );
 }
+
+export default Homepage;
