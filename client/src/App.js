f972a329c06d30c07e81c4289d830c98b882fe9d
Alice Bryer <alicebryer123@gmail.com>
2022-09-03 12:54:09 +0100

    basic layout for facilities:hard-coded

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Homepage from "./components/Homepage";
 import SignUp from "./components/Signup";
 import Login from "./components/Login";
+import Facilities from "./components/FacilitiesPage"
 
 function App() {
   useEffect(() => {
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/Facilities" element={<Facilities />} />
         </Routes>
       </Wrapper>
     </Router>
---
+++ client/src/components/FacilitiesPage.js
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+const ActionAreaCard = () => {
+  return (
+<div className='facilities-card-container'>
+    <div className='facilities-card'>
+    <Card sx={{ maxWidth: 445 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://www.kingfisher.oldham.sch.uk/wp-content/uploads/2021/01/IMG_4202.jpeg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Hydrotherapy Pool
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Heal tired muscles whilst lowering your stress levels, in our blissful hydrotherapy pool. Swim under our massage jets, or unwind in our bubbling hot tub.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </div>
+   <div className='facilities-card'>
+      <Card sx={{ maxWidth: 445 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://www.kingfisher.oldham.sch.uk/wp-content/uploads/2021/01/IMG_4202.jpeg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+            Hydrotherapy Pool
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            Heal tired muscles whilst lowering your stress levels, in our blissful hydrotherapy pool. Swim under our massage jets, or unwind in our bubbling hot tub.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      </div>
+      <div className='facilities-card'>
+    <Card sx={{ maxWidth: 445 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://www.kingfisher.oldham.sch.uk/wp-content/uploads/2021/01/IMG_4202.jpeg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Hydrotherapy Pool
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Heal tired muscles whilst lowering your stress levels, in our blissful hydrotherapy pool. Swim under our massage jets, or unwind in our bubbling hot tub.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </div>
+    <div className='facilities-card'>
+    <Card sx={{ maxWidth: 445 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://www.kingfisher.oldham.sch.uk/wp-content/uploads/2021/01/IMG_4202.jpeg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Hydrotherapy Pool
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Heal tired muscles whilst lowering your stress levels, in our blissful hydrotherapy pool. Swim under our massage jets, or unwind in our bubbling hot tub.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </div>
+    <div className='facilities-card'>
+    <Card sx={{ maxWidth: 445 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://www.kingfisher.oldham.sch.uk/wp-content/uploads/2021/01/IMG_4202.jpeg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Hydrotherapy Pool
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Heal tired muscles whilst lowering your stress levels, in our blissful hydrotherapy pool. Swim under our massage jets, or unwind in our bubbling hot tub.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </div>
+    <div className='facilities-card'>
+    <Card sx={{ maxWidth: 445 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://www.kingfisher.oldham.sch.uk/wp-content/uploads/2021/01/IMG_4202.jpeg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Hydrotherapy Pool
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Heal tired muscles whilst lowering your stress levels, in our blissful hydrotherapy pool. Swim under our massage jets, or unwind in our bubbling hot tub.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </div>
+      </div>
+    );
+}
+
+
+
+export default ActionAreaCard;
