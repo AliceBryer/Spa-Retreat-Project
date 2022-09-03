12c1042c19959a7d4168c3805e1b69eca9d27c3b
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-03 13:35:18 +0100

    creating typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,28 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+type Order {
+        _id: ID
+        purchaseDate: String
+        products: [Product]
+      }
+
+type Facilities {
+        _id: ID
+        name: String
+        description: String
+        pictureurl: String
+        price: Float
+}      
+`;
+
 
 module.exports = typeDefs;
