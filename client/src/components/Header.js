71c2ce1f711c32c6d771d0b0d9b4a28c1203b241
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-05 12:24:48 +0100

    import useState and Link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
