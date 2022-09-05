ef85423e7d75b2ab022cf5986f2e81e13d79d04d
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-05 13:57:08 +0100

    import functions from utils

---
+++ client/src/components/Wishlist.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Auth from "../utils/auth";
+import { removeTreatmentId } from "../utils/localStorage";
+
+import { useQuery, useMutation } from "@apollo/client";
+// name of the functions to be edited after getting the code from backend team
+import { GET_USER } from "../utils/queries";
+import { REMOVE_WISHLIST } from "../utils/mutations";
+
+const Wishlist = () => {};
