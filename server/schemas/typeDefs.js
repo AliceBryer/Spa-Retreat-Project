f73eadf816cd9b8e644a035b1058164d344068ff
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-05 22:16:24 +0100

    started typedef code resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,4 +2,22 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql``;
 
+type Facilities {
+    _id: ID
+    name: String
+}
+
+type Treatment {
+    _id :ID
+    name: String 
+    description: String
+    image: String 
+    quantity: Int
+    price:
+}
+
+type Order {
+    _id: ID
+    purchaseDate: String
+;
 module.exports = typeDefs;
