cebc373171356ede5be29b07a22a0113cb41998e
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-05 14:02:05 +0100

    function to del treatment from wishlist

--- client/src/components/Wishlist.js
+++ client/src/components/Wishlist.js
@@ -8,4 +8,25 @@ import { useQuery, useMutation } from "@apollo/client";
 import { GET_USER } from "../utils/queries";
 import { REMOVE_WISHLIST } from "../utils/mutations";
 
-const Wishlist = () => {};
+const Wishlist = () => {
+  const { loading, data } = useQuery(GET_USER);
+  const [removeTreatment] = useMutation(REMOVE_WISHLIST);
+
+  const userData = data?.user || {};
+
+  const handleDeleteTreatment = async (treatmentId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+    try {
+      await removeTreatment({
+        variables: { treatmentId },
+      });
+      removeTreatmentId(treatmentId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
