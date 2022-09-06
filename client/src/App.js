91cd4c4ad43aea4b63ebb6f220649214e8431d72
Alice Bryer <alicebryer123@gmail.com>
2022-09-06 12:34:56 +0100

    hard-coded-layout with minimal styling

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Homepage from "./components/Homepage";
 import SignUp from "./components/Signup";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
+import Treatment from "./components/TreatmentPage"
 
 function App() {
   useEffect(() => {
@@ -22,6 +23,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+
+          <Route path="/Treatment" element={<Treatment />} />
         </Routes>
       </Wrapper>
       <Footer />
