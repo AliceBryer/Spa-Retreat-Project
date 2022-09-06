bf98f526d787ca703c550b95911538daa8fa96a3
KeyurMistry <keyurmist@gmail.com>
2022-09-06 10:32:27 +0100

    adjusted aboutus layout

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,18 +1,20 @@
-import React from "react";
+import candle from "../img/candle.avif";
+import diffuser from "../img/diffuser.avif";
+import massage from "../img/massage.avif";
 
-import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import PinterestIcon from "@mui/icons-material/Pinterest";
 
 const AboutUs = () => {
   return (
-    <div id="model5">
+    <div id="model4">
+      <h1 className="model-title">Team Model 4</h1>
       <div className="divider"></div>
       <div className="members">
         <div className="member">
-          <img width={400} src={""} />
+          <img width={200} height={200} src={candle} />
           <div className="description">
-            <h1>Ahmed ALAMI</h1>
+            <h1>Angela Wong</h1>
             <h2>CEO</h2>
             <p>
               Consectetuer adipiscing elit, sed diam nonummy nibh euismod
@@ -20,14 +22,13 @@ const AboutUs = () => {
               sit amet consectet.
             </p>
             <div className="social-media">
-              <InstagramIcon />
+              <GitHubIcon />
               <LinkedInIcon />
-              <PinterestIcon />
             </div>
           </div>
         </div>
         <div className="member">
-          <img width={400} src={""} />
+          <img width={200} height={200} src={massage} />
           <div className="description">
             <h1>Omar Nadir</h1>
             <h2>CEO</h2>
@@ -37,14 +38,13 @@ const AboutUs = () => {
               sit amet consectet.
             </p>
             <div className="social-media">
-              <InstagramIcon />
+              <GitHubIcon />
               <LinkedInIcon />
-              <PinterestIcon />
             </div>
           </div>
         </div>
         <div className="member">
-          <img width={400} src={""} />
+          <img width={200} height={200} src={diffuser} />
           <div className="description">
             <h1>Khaled MAHER</h1>
             <h2>CEO</h2>
@@ -54,9 +54,8 @@ const AboutUs = () => {
               sit amet consectet.
             </p>
             <div className="social-media">
-              <InstagramIcon />
+              <GitHubIcon />
               <LinkedInIcon />
-              <PinterestIcon />
             </div>
           </div>
         </div>
