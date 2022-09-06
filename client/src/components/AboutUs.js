6647a7c70546c34964dd5be6c8ff28637360a683
KeyurMistry <keyurmist@gmail.com>
2022-09-06 10:00:09 +0100

    added css for about us

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-import "./Model4.css";
-import Member1 from "../Assets/Member1.jpg";
-import Member2 from "../Assets/Member2.jpg";
-import Member3 from "../Assets/Member3.png";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import PinterestIcon from "@mui/icons-material/Pinterest";
