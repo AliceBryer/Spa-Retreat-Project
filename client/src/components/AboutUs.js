3f442b1ec8a8bd9a4e542e9b29b5bfb80ab53dd5
KeyurMistry <keyurmist@gmail.com>
2022-09-06 09:54:32 +0100

    imported mui components

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
-import Auth from "../utils/auth";
-import { useMutation } from "@apollo/client";
+import "./Model4.css";
+import Member1 from "../Assets/Member1.jpg";
+import Member2 from "../Assets/Member2.jpg";
+import Member3 from "../Assets/Member3.png";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PinterestIcon from "@mui/icons-material/Pinterest";
 
 const AboutUs = () => {
   return <></>;
