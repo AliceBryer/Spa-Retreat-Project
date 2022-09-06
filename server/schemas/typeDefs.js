bf2e49e9a431210a28886cd1ce2b9305148e7d87
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-06 17:59:24 +0100

    mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,6 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql``;
-
 type Facilities {
     _id: ID
     name: String
@@ -27,6 +26,25 @@ type User {
     lastName: String
     email: String
     orders: [Order]
+  }
+
+  type Query {
+    Facilities: [Facilities]
+    treatment (facilities: ID, name: String): [Treatment]
+    treatment (_id: ID!): Treatment
+    user: User
+    order(_id: ID!): Order
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    addTreatment (_id: ID!, name:String, quantity: Int!): Treatment
+    updateTreatment(_id: ID!, name:String, quantity: Int!): Treatment 
+    deleteTreatment(_id: ID!, name:String, quantity: Int!): Treatment 
+    addOrder(treatment: [ID]!): Order
+    updateOrder(_id: ID!, purchaseDate:String, quantity: Int!): Treatment
+    login(email: String!, password: String!): Auth
   }
 ;
 module.exports = typeDefs;
