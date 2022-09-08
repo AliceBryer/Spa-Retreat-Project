3321450bcee193a4c5a6b289884fd925545a799d
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-08 17:27:48 +0100

    mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,20 @@ const {} = require("../models");
 
 const resolvers = {
   Query: {},
-  Mutation: {},
+
+  Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    addTreatment (_id: ID!, name:String, quantity: Int!): Treatment
+    updateTreatment(_id: ID!, name:String, quantity: Int!): Treatment 
+    deleteTreatment(_id: ID!, name:String, quantity: Int!): Treatment 
+    addOrder(treatment: [ID]!): Order
+    updateOrder(_id: ID!, purchaseDate:String, quantity: Int!): Treatment
+    login(email: String!, password: String!): Auth
+  }
+
+  },
 };
+
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,42 +1,9 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
-type Facilities {
-    _id: ID
-    name: String
-}
 
-type Treatment {
-    _id :ID
-    name: String 
-    description: String
-    image: String 
-    quantity: Int
-    price:
-}
 
-type Order {
-    _id: ID
-    purchaseDate: String
-    treatment: [Treatment]
-}
-type User {
-    _id: ID
-    firstName: String
-    lastName: String
-    email: String
-    orders: [Order]
-  }
 
-  type Query {
-    Facilities: [Facilities]
-    treatment (facilities: ID, name: String): [Treatment]
-    treatment (_id: ID!): Treatment
-    user: User
-    order(_id: ID!): Order
-  }
-
-  type Mutation {
+   Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     addTreatment (_id: ID!, name:String, quantity: Int!): Treatment
