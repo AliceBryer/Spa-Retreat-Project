c185c9b67489ea99ffa5a4f6acbf18d50c78cb4e
Alice Bryer <alicebryer123@gmail.com>
2022-09-08 10:36:14 +0100

    added map function with all seeded data hard-coded.

--- client/src/components/FacilitiesPage.js
+++ client/src/components/FacilitiesPage.js
@@ -20,6 +20,35 @@ const dummyData = [
     picture:
       "https://images.unsplash.com/photo-1500840216050-6ffa99d75160?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=897&q=80",
   },
+  {
+    name: "Hydrotherapy Pool",
+    description:
+      "Heal tired muscles whilst lowering your stress levels, in our blissful hydrotherapy pool. Swim under our massage jets, or unwind in our bubbling hot tub. ",
+    pictureURL:
+      "https://images.unsplash.com/photo-1470376619031-a6791e534bf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+
+  {
+    name: "Saltwater Pool",
+    description:
+      "Promote the bodies automatic relaxtion in our mesmerising saltwater pool. Float amongst the crystal clear waters, discovering our starlit cave and poolside loungers.",
+    pictureURL:
+      "https://images.unsplash.com/photo-1427097829427-56a905bf7004?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2FsdHdhdGVyJTIwcG9vbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    name: "Detox Centre",
+    description:
+      "Remove harmful toxins from the body by stimulating your circulatory system in our experience showers or tropical sauna.",
+    pictureURL:
+      "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  {
+    name: "The Atrium",
+    description:
+      "Kick back in our open plan area and make the most of our detoxing juices and healthy meals and snacks.",
+    pictureURL:
+      "https://images.unsplash.com/photo-1559625482-34b91d929ce8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  },
 ];
 
 const ActionAreaCard = () => {
