36e2c395ab7833e970349a5d4c232c9614352b9f
KeyurMistry <keyurmist@gmail.com>
2022-09-08 18:24:29 +0100

    using material UI for footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,64 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        404 GetAway
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 function Footer() {
   return (
-    <footer className="footer">
-      <h5>©Team Super Awesome</h5>
-      <div className="footer-wrap">
-        <ul className="footer-list">
-          <li>
-            <NavLink to="/AboutUs">About Us</NavLink>
-            <NavLink to="/About">About</NavLink>
-          </li>
-        </ul>
-      </div>
-    </footer>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <CssBaseline />
+      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Sticky footer
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {"Pin a footer to the bottom of the viewport."}
+          {"The footer will move as the main element of the page grows."}
+        </Typography>
+        <Typography variant="body1">Sticky footer placeholder.</Typography>
+      </Container>
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
   );
 }
 export default Footer;
