967f621d66c66f8ac870d4b12a0984370420611b
mohamed hassan <mohamedisaaq0@gmail.com>
2022-09-08 19:40:30 +0100

    added missing key in user model

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,12 @@ const { Schema } = mongoose;
 const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-  name: {
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
     type: String,
     required: true,
     trim: true,
