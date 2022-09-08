898ff1dbc8219dcc5518c8c813650b63868c9a48
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-08 11:56:54 +0100

    add wishlist model to index.js

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,6 @@ const Treatments = require("./Treatments");
 const Facilities = require("./Facilities");
 const Order = require("./Order");
 const Review = require("./review");
+const Wishlist = require("./wishlist");
 
-module.exports = { User, Treatments, Facilities, Order, Review };
+module.exports = { User, Treatments, Facilities, Order, Review, Wishlist };
