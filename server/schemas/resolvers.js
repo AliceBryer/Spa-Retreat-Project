fa3a0793c212c0ea8ad41368fdcfd314e9bc6694
mohamed hassan <mohamedisaaq0@gmail.com>
2022-09-09 02:05:16 +0100

    fixed adduser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -74,7 +74,7 @@ const resolvers = {
     //   return { session: session.id };
   },
   Mutation: {
-    addUser: async (parent, { name, email, password }) => {
+    addUser: async (parent, { firstName, lastName, email, password }) => {
       const user = await User.create({ firstName, lastName, email, password });
       const token = signToken(user);
       return { token, user };
@@ -88,11 +88,6 @@ const resolvers = {
       if (!correctPw) {
         throw new AuthenticationError("Incorrect credentials");
       }
-      const token = signToken(user);
-      return { token, user };
-    },
-    addUser: async (parent, { name, email, password }) => {
-      const user = await User.create({ name, email, password });
       const token = signToken(user);
       return { token, user };
     },
@@ -122,18 +117,6 @@ const resolvers = {
         { $inc: { quantity: decrement } },
         { new: true }
       );
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-      if (!user) {
-        throw new AuthenticationError("Incorrect credentials");
-      }
-      const correctPw = await user.isCorrectPassword(password);
-      if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
-      }
-      const token = signToken(user);
-      return { token, user };
     },
   },
 };
