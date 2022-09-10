1c6a0c2a62b4ec02bc2b5f691a027582409136db
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-10 04:27:34 +0100

    update user and stipe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,6 +8,7 @@ const {
 } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 const resolvers = {
   Query: {
@@ -52,36 +53,36 @@ const resolvers = {
       throw new AuthenticationError("Not logged in");
     },
 
-    // checkout: async (parent, args, context) => {
-    //   const url = new URL(context.headers.referer).origin;
-    //   const order = new Order({ treatments: args.treatments });
-    //   const line_items = [];
-    //   const { treatments } = await order.populate("treatments");
-    //   // check !
-    //   for (let i = 0; i < treatments.length; i++) {
-    //     const treatment = await stripe.treatments.create({
-    //       name: treatments[i].name,
-    //       description: treatments[i].description,
-    //       images: [`${url}/images/${treatments[i].image}`],
-    //     });
-    //     const price = await stripe.prices.create({
-    //       treatment: treatment.id,
-    //       unit_amount: treatments[i].price * 100,
-    //       currency: "GBP",
-    //     });
-    //     line_items.push({
-    //       price: price.id,
-    //       quantity: 1,
-    //     });
-    //   }
-    //   const session = await stripe.checkout.sessions.create({
-    //     payment_method_types: ["card"],
-    //     line_items,
-    //     mode: "payment",
-    //     success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-    //     cancel_url: `${url}/`,
-    //   });
-    //   return { session: session.id };
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ treatments: args.treatments });
+      const line_items = [];
+      const { treatments } = await order.populate("treatments");
+      // check !
+      for (let i = 0; i < treatments.length; i++) {
+        const treatment = await stripe.treatments.create({
+          name: treatments[i].name,
+          description: treatments[i].description,
+          images: [`${url}/images/${treatments[i].image}`],
+        });
+        const price = await stripe.prices.create({
+          treatment: treatment.id,
+          unit_amount: treatments[i].price * 100,
+          currency: "GBP",
+        });
+        line_items.push({
+          price: price.id,
+          quantity: 1,
+        });
+      }
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items,
+        mode: "payment",
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`,
+      });
+      return { session: session.id };
   },
 
   Mutation: {
@@ -121,19 +122,34 @@ const resolvers = {
       return User.findOneAndDelete({ _id });
     },
 
-    // TODO: add treatment
-    // no need?
-
-    // update treatment: no need?
-    // updateTreatment: async (parent, { _id, quantity }) => {
-    //   const decrement = Math.abs(quantity) * -1;
-    //   return await Product.findByIdAndUpdate(
-    //     _id,
-    //     { $inc: { quantity: decrement } },
-    //     { new: true }
-    //   );
-    // },
-
+  
+    updateTreatment: async (parent, { _id, quantity }) => {
+      const decrement = Math.abs(quantity) * -1;
+      return await Treatment.findByIdAndUpdate(
+        _id,
+        { $inc: { quantity: decrement } },
+        { new: true }
+      );
+    },
+    
+  login: async (parent, { email, password }) => {
+    const user = await User.findOne({ email });
+  
+    if (!user) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+  
+    const correctPw = await user.isCorrectPassword(password);
+  
+    if (!correctPw) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+  
+    const token = signToken(user);
+  
+    return { token, user };
+  
+  },
     // TODO: del treatment
     // no need?
 
