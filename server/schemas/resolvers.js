02023af2fc6090e3d6e7baa4afa3a70476c6d72e
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-11 22:59:21 +0100

    fix query me and tested

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,7 +13,8 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate(Wishlist);
+        return User.findOne({ _id: context.user._id });
+        // .populate(Wishlist);
       }
       throw new AuthenticationError("You need to be logged in!");
     },
