365a6413455d659dbc8a1193d7d5c1448c28d572
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-13 21:19:31 +0100

    added grid and profile pic syling

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -8,6 +8,7 @@ import {
   Typography,
 } from "@material-ui/core";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 const useStyles = makeStyles((theme) => ({
   text: {
@@ -37,48 +38,55 @@ export default function UserCard(props) {
 
   return (
     <Card variant="outlined" className={classes.container}>
-      <CardMedia align="center">
-        <Avatar
-          alt="Remy Sharp"
-          src={props?.content?.picture?.large}
-          className={classes.large}
-        />
-      </CardMedia>
-      <CardContent align="left">
-        <Typography
-          className={classes.text}
-          color="textSecondary"
-          variant="h6"
-          align="center"
-        >
-          {"First Name: Mohamed "}
-        </Typography>
-        <Typography
-          className={classes.text}
-          color="textSecondary"
-          variant="h6"
-          align="center"
-        >
-          {"First Name: Mohamed "}
-        </Typography>
-        <Typography
-          className={classes.text}
-          color="textSecondary"
-          variant="h6"
-          align="center"
-        >
-          {"Email: Mohamed@test.com "}
-        </Typography>
-        <Button
-          fullWidth
-          variant="contained"
-          sx={{ mt: 2, mb: 2 }}
-          color="error"
-          onClick={handleButtonClick}
-        >
-          Delete Account
-        </Button>
-      </CardContent>
+      <Grid container spacing={2}>
+        <Grid item xs={4} id="profilepic">
+          <CardMedia align="left">
+            <Avatar
+              alt="Remy Sharp"
+              src={props?.content?.picture?.large}
+              className={classes.large}
+            />
+          </CardMedia>
+        </Grid>
+
+        <Grid item xs={8} id>
+          <CardContent align="left">
+            <Typography
+              className={classes.text}
+              color="textSecondary"
+              variant="h6"
+              align="center"
+            >
+              {"First Name: Mohamed "}
+            </Typography>
+            <Typography
+              className={classes.text}
+              color="textSecondary"
+              variant="h6"
+              align="center"
+            >
+              {"First Name: Mohamed "}
+            </Typography>
+            <Typography
+              className={classes.text}
+              color="textSecondary"
+              variant="h6"
+              align="center"
+            >
+              {"Email: Mohamed@test.com "}
+            </Typography>
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 2, mb: 2 }}
+              color="error"
+              onClick={handleButtonClick}
+            >
+              Delete Account
+            </Button>
+          </CardContent>
+        </Grid>
+      </Grid>
     </Card>
   );
 }
