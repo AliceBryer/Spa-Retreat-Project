61b0d9b175b4446217bfedbb6f6f4c3818252181
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-13 15:13:59 +0100

    set up for JWT token, tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import Wrapper from "./components/Wrapper";
 import "./App.css";
 
@@ -15,29 +23,50 @@ import About from "./components/About";
 import AboutUs from "./components/AboutUs";
 import MyAccount from "./components/MyAccount";
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   useEffect(() => {
     document.title = "Spa Retreat";
   }, []);
 
   return (
-    <Router>
-      <Header />
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/treatment" element={<Treatment />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path="/facilities" element={<Facilities />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/aboutUs" element={<AboutUs />} />
-          <Route path="/myaccount" element={<MyAccount />} />
-        </Routes>
-      </Wrapper>
-      <Footer />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+        <Wrapper>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/treatment" element={<Treatment />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/facilities" element={<Facilities />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/aboutUs" element={<AboutUs />} />
+            <Route path="/myaccount" element={<MyAccount />} />
+          </Routes>
+        </Wrapper>
+        <Footer />
+      </Router>
+    </ApolloProvider>
   );
 }
 
