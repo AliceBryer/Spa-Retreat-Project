23c95495a3eae1561eb5ff48835bbe5559253f7a
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-14 01:58:09 +0100

    remove treatment from wishlist done and tested

--- client/src/components/Wishlist.js
+++ client/src/components/Wishlist.js
@@ -1,43 +1,25 @@
 import React from "react";
 
-// import Auth from "../utils/auth";
-// import { removeTreatmentId } from "../utils/localStorage";
-
-import {
-  useQuery,
-  // , useMutation
-} from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 // name of the functions to be edited after getting the code from backend team
 import { QUERY_WISHLIST } from "../utils/queries";
-// import { DEL_WISHLIST } from "../utils/mutations";
+import { DEL_WISHLIST } from "../utils/mutations";
 
 const Wishlist = () => {
   const { data } = useQuery(QUERY_WISHLIST);
-  // const [removeTreatmentFromWishlist] = useMutation(DEL_WISHLIST);
-
-  // const handleDeleteTreatment = async (_id) => {
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //   if (!token) {
-  //     return false;
-  //   }
-  //   try {
-  //     await removeTreatmentFromWishlist({
-  //       variables: { _id },
-  //     });
-  //     removeTreatmentId(_id);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  // testing buttons
-  function addCart() {
-    alert("You clicked add cart!");
-  }
   const wishlistData = data?.wishlist.treatments ?? [];
 
-  console.log(wishlistData);
+  const [removeTreatmentFromWishlist] = useMutation(DEL_WISHLIST);
+  const handleRemoveTreatment = async (treatment) => {
+    try {
+      const { data } = await removeTreatmentFromWishlist({
+        variables: { treatment },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const wishlistCard = wishlistData.map((treatment) => {
     return (
       <div key={treatment._id} class="wishlist-card">
@@ -50,11 +32,14 @@ const Wishlist = () => {
         <div className="btn-container">
           <button
             className="btn btn-del"
-            // onClick={() => handleDeleteTreatment(treatment._id)}
+            onClick={() => handleRemoveTreatment(treatment._id)}
           >
             Remove
           </button>
-          <button className="btn btn-addCart" onClick={addCart}>
+          <button
+            className="btn btn-addCart"
+            // onClick={addCart}
+          >
             Add to Cart
           </button>
         </div>
