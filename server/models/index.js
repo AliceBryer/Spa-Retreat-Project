e7fb76ce048bc3a8d70c981f6a074e1eb20ef2bb
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-15 21:22:45 +0100

    fix model

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const User = require("./user");
 const Treatments = require("./Treatments");
 const Facilities = require("./Facilities");
 const Order = require("./Order");
-const Review = require("./review");
+const Review = require("./Review");
 const Wishlist = require("./wishlist");
 
 module.exports = { User, Treatments, Facilities, Order, Review, Wishlist };
