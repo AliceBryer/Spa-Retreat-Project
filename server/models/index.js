ebd7aa61740f28a99c682c1385c9b3a6aa21ce2b
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-09-15 21:23:21 +0100

    fix Wishlist model

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,6 @@ const Treatments = require("./Treatments");
 const Facilities = require("./Facilities");
 const Order = require("./Order");
 const Review = require("./Review");
-const Wishlist = require("./wishlist");
+const Wishlist = require("./Wishlist");
 
 module.exports = { User, Treatments, Facilities, Order, Review, Wishlist };
